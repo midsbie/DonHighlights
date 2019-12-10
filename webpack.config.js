b9299fc7143b2e7f6a8d6ba194dcc0a2f1e81e09
Miguel Guedes <miguel.a.guedes@gmail.com>
2019-12-10 08:27:48 +0000

    build: upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,144 +1,106 @@
 /* global process */
 
-const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const env = process.env; // eslint-disable-line no-process-env
-const isProduction = env.NODE_ENV === 'production';
+const isProd = env.NODE_ENV === 'production';
 
-function makeExampleConfig(name) {
+function makeConfig() {
   const config = {
-    entry: {
-      // We always assume that the example's entry filename is `main.js`
-      [name]: `./examples/${name}/main.js`,
-    },
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? false : 'source-map',
+    entry: { 'html-highlighter': './src/main.js' },
     output: {
-      path: path.join(__dirname, 'dist/examples', name),
-      filename: '[name].js',
+      path: path.join(__dirname, 'dist'),
+      filename: isProd ? '[name].min.js' : '[name].js',
+      library: '[name]',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
     },
     module: {
       rules: [
-        jsLoader,
-        { test: /\.css$/, loader: 'style-loader!css-loader' },
-        { test: /\.json$/, loader: 'json-loader' },
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                // This is a feature of `babel-loader` for webpack (not Babel itself), which enables
+                // caching results in `./node_modules/.cache/babel-loader/` directory for faster
+                // rebuilds.
+                cacheDirectory: true,
+                cacheCompression: isProd,
+                compact: isProd,
+              },
+            },
+          ],
+        },
       ],
     },
     plugins: [
       new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(env.NODE_ENV),
-        },
+        __DEV__: !isProd,
+        __PROD__: isProd,
       }),
-      new HtmlWebpackPlugin({
-        title: `HTML Highlighter -- ${name} example`,
-        template: `./examples/${name}/main.html`,
-        filename: 'index.html',
-      }),
+      new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf8')),
     ],
-    devtool: '#source-map',
   };
 
-  if (isProduction) {
-    config.module.rules = config.module.rules.concat(linters);
-  }
+  if (!isProd) return config;
+
+  config.optimization = {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            // We want terser to parse ecma 8 code. However, we don't want it to apply any
+            // minification steps that turns valid ecma 5 code into invalid ecma 5 code. This is
+            // why the 'compress' and 'output' sections only apply transformations that are ecma
+            // 5 safe.
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            //
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            //
+            // Pending further investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          // TODO: unclear what this does; look in create-react-app for clues.
+          keep_classnames: false,
+          keep_fnames: false,
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true,
+          },
+        },
+        sourceMap: false,
+      }),
+    ],
+  };
 
   return config;
 }
 
-const linters = [
-  {
-    enforce: 'pre',
-    test: /\.js$/,
-    exclude: /node_modules|examples/,
-    loader: 'eslint-loader',
-  },
-];
-
-const jsLoader = {
-  test: /\.js$/,
-  loader: 'babel-loader',
-  exclude: /node_modules/,
-  query: {
-    cacheDirectory: true,
-  },
-};
-
-const lib = {
-  entry: { htmlhighlighter: './src/main.js' },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
-  module: {
-    rules: [jsLoader].concat(linters),
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      PRODUCTION: isProduction,
-      BROWSER: true,
-      TEST: false,
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(true),
-    new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf8')),
-  ],
-  devtool: '#source-map',
-};
-
-const assets = {
-  entry: ['./examples/media/images/logo.png'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'assets.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.json$/,
-        loader: 'file-loader?name=examples/data/[name].[ext]',
-      },
-      {
-        test: /\.png$/,
-        loader: 'file-loader?name=[path][name].[ext]',
-      },
-    ],
-  },
-};
-
-if (isProduction) {
-  // Documentation disabled for now.
-  // --
-  // lib.module.rules.push({
-  //   enforce: "post",
-  //   test: /\.js$/,
-  //   exclude: /node_modules/,
-  //   loader: "documentation"
-  // });
-  //
-  // lib.documentation = {
-  //   format: "html",
-  //   output: "html_highlighter.js.html"
-  // };
-
-  // Append `.min` suffix after file name, drop duplicate symbols and minify
-  // build artifacts.
-  lib.output.filename = '[name].min.js';
-  lib.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-    })
-  );
-}
-
-const examples = [makeExampleConfig('monolith')];
-let build = [lib];
-if (!isProduction) {
-  build = build.concat(examples, [assets]);
-}
-
-module.exports = build;
+module.exports = makeConfig();
