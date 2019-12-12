ec1303902c00b43fa8b2cdac88bac7b994dadbbf
Miguel Guedes <miguel.a.guedes@gmail.com>
2019-12-12 14:13:53 +0000

    src: provide isHighlight as standalone function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export type { HighlightTextQuery, HighlightXPathQuery } from './typedefs';
 export { createHighlighter } from './factory';
 export type { Options } from './factory';
 export type { RangeXPathDescriptor } from './TextRange';
+export * from './HighlightRenderer'; // skipping default
 
 export { default as DOMHighlighter } from './DOMHighlighter';
 export { default as TextFinder } from './TextFinder';
